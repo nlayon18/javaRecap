9b729a78c6850303d45bcc1ba4dc22d763ca4f03
Naomi Layon <nlayon@student.bridgew.edu>
2022-09-20 16:32:41 -0400

    My attempt at Step 3!

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner input;
+        Scanner file;
         //Verifying if objects are working below.
         /**
         Patient p1 = new Patient("Mark Markus",23, "Dr. Peter Mitchell");
@@ -18,17 +19,27 @@ public class Main {
         ArrayList<String> ArrayList = new ArrayList<>();
       //Scanners will l8r represent patients.txt... will be done in try-catch method
         try {
-            input = new Scanner(new File("C:\\Users\\Naomi Layon\\IdeaProjects\\javaRecap\\sr" +
+            file = new Scanner(new File("C:\\Users\\Naomi Layon\\IdeaProjects\\javaRecap\\sr" +
                     "c\\patients.txt"));
             //Scanner p2 = new Scanner();
 
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e){
             System.out.println("Invalid File Path!");
             return;
         }
-        System.out.println(input);
+        System.out.println(file);
 
         // UPDATE: Finished "Starting Main Method" ... Need to start back off on Step 3
+
+        while(file.hasNextLine()){
+            String [] thisIndexAge = file.nextLine().split(",");
+            String age = thisIndexAge [1];
+            //System.out.println(file.nextLine());
+            //System.out.println(Arrays.toString(file.nextLine().split(",")));
+            //String[] thisLine = patient.nextLine.split(",");
+            System.out.println(age);
+
+        }
+
     }
 }
